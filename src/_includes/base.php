4c1aa0eb05b57430c2e6e3ee10e00801a8ea0984
PaulaWhitefield <contact@paulawhitefield.com>
2023-09-02 16:45:06 +0100

    Updated Staticly URLs

--- src/_includes/base.php
+++ src/_includes/base.php
@@ -6,9 +6,9 @@
         
         <title><?php if( isset( $page_title ) && $page_title ) echo $page_title . " - "; ?>Little Innocent Angel</title>
         
-        <link rel="stylesheet" type="text/css" href="https://w3css.staticly.ict.rocks/4.15/w3.css">
-        <link rel="stylesheet" type="text/css" href="https://w3css.staticly.ict.rocks/w3-colors-metro.css">
-        <link rel="stylesheet" type="text/css" href="https://webfonts.staticly.ict.rocks/nunito/nunito.css">
+        <link rel="stylesheet" type="text/css" href="https://staticly.cc/w3css/4.15/w3.css">
+        <link rel="stylesheet" type="text/css" href="https://staticly.cc/w3css/w3-colors-metro.css">
+        <link rel="stylesheet" type="text/css" href="https://staticly.cc/webfonts/nunito/nunito.css">
     </head>
     
     <body class="w3-black">
